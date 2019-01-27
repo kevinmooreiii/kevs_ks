5934b8e6d513d349434cead0dace3e85a261e450
kmoore <srkevinmoore@gmail.com>
2019-01-27 08:47:04 -0600

    changes

--- lib/projection.py
+++ lib/projection.py
@@ -1,6 +1,6 @@
-'''
+"""
   Projects out translation, external rotation, and hindered rotor motions from the Hessian
-'''
+"""
 
 import sys
 import numpy as np
@@ -8,101 +8,103 @@ import numpy as np
 import molecule
 
 def project_Hessian(masses, xyz, H):
-  ''' Project the motions out of the Hessian. '''
+    """ Project the motions out of the Hessian. """
 
-  # Calculate the vectors associated with translation, external rotation, and hindered internal rotation 
-  TransMat   = build_translation_vectors(masses)
-  ExtRotMat  = build_ext_rotation_vectors(masses, xyz)
-  #HindRotMat = build_hind_rotation_vectors()
-  NHindRot = 0
-  Natoms = len(mass)
+    # Calculate the vectors associated with translation, external rotation, and hindered internal rotation 
+    TransMat   = build_translation_vectors(masses)
+    ExtRotMat  = build_ext_rotation_vectors(masses, xyz)
+    # HindRotMat = build_hind_rotation_vectors()
+    NHindRot = 0
+    Natoms = len(mass)
 
-  # Compound the motional vectors into a vector D
-  D = np.column_stack( (TransMat, ExtRotMat) )
+    # Compound the motional vectors into a vector D
+    D = np.column_stack( (TransMat, ExtRotMat) )
 
-  #print(TransMat)
-  print(ExtRotMat)
-  #print(HindRotMat)
-  #print(D)
+    # print(TransMat)
+    print(ExtRotMat)
+    # print(HindRotMat)
+    # print(D)
 
-  # Orthogonalize D matrix
-  #Dortho = orthogonalize_D_matrix(D)
+    # Orthogonalize D matrix
+    # Dortho = orthogonalize_D_matrix(D)
   
-  # Build the Projection Matrix (P)
-  Ds = np.dot(D, D.T)
-  #print(Ds)
-  I = np.identity(3*Natoms)
-  P = I - Ds
+    # Build the Projection Matrix (P)
+    Ds = np.dot(D, D.T)
+    #print(Ds)
+    I = np.identity(3*Natoms)
+    P = I - Ds
 
-  # Project out the modes out of the Hessian Matrix 
-  #Hproj = np.dot(P.T * H * P)
+    # Project out the modes out of the Hessian Matrix 
+    # Hproj = np.dot(P.T * H * P)
 
-  # Diagonalize the Projected Hessian
-  #lam, L = np.linalg.eig(Hproj)
-  lam = 1.0
+    # Diagonalize the Projected Hessian
+    # lam, L = np.linalg.eig(Hproj)
+    lam = 1.0
 
-  return lam
+    return lam
 
 
 def build_translation_vectors(masses):
-  ''' Builds the vectors that describe the translation of the molecule.
-  '''    
+    """ Builds the vectors that describe the translation of the molecule.
+    """    
 
-  # Calculate number of atoms
-  Natoms = len(masses)
+    # Calculate number of atoms
+    Natoms = len(masses)
 
-  # Build the vectors for translation
-  Transx, Transy, Transz = np.zeros(3*Natoms), np.zeros(3*Natoms), np.zeros(3*Natoms) 
-  for i in range(Natoms):
-    Transx[ 3*i : 3*i+3 ] = tuple( np.sqrt(masses[i]) * elem for elem in [ 1.0, 0.0, 0.0 ] )
-    Transy[ 3*i : 3*i+3 ] = tuple( np.sqrt(masses[i]) * elem for elem in [ 0.0, 1.0, 0.0 ] )
-    Transz[ 3*i : 3*i+3 ] = tuple( np.sqrt(masses[i]) * elem for elem in [ 0.0, 0.0, 1.0 ] )
+    # Build the vectors for translation
+    Transx = np.zeros(3*Natoms) 
+    Transy = np.zeros(3*Natoms) 
+    Transz = np.zeros(3*Natoms) 
+    for i in range(Natoms):
+        Transx[3*i:3*i+3] = tuple(np.sqrt(masses[i]) * elem for elem in [1.0, 0.0, 0.0])
+        Transy[3*i:3*i+3] = tuple(np.sqrt(masses[i]) * elem for elem in [0.0, 1.0, 0.0])
+        Transz[3*i:3*i+3] = tuple(np.sqrt(masses[i]) * elem for elem in [0.0, 0.0, 1.0])
 
-  # Put vectors into a matrix
-  Trans = np.column_stack( (Transx, Transy, Transz) )
+    # Put vectors into a matrix
+    Trans = np.column_stack( (Transx, Transy, Transz) )
 
-  return Trans
+    return Trans
 
 
 def build_ext_rotation_vectors(masses, xyz):
-  ''' Builds the vectors that describe the external rotation of the molecule. 
-  '''    
+    """ Builds the vectors that describe the external rotation of the molecule. 
+    """    
 
-  # Calculate number of atoms
-  Natoms = len(masses)
+    # Calculate number of atoms
+    Natoms = len(masses)
  
-  # Calculate the moment-of-inertia and center-of-mass
-  Rcom = molecule.calc_center_of_mass(masses, xyz)
-  Ip, Xp = molecule.calc_moment_of_inertia(masses, xyz)
+    # Calculate the moment-of-inertia and center-of-mass
+    Rcom = molecule.calc_center_of_mass(masses, xyz)
+    Ip, Xp = molecule.calc_moment_of_inertia(masses, xyz)
 
-  # Obtain vectors for the projection of ...
-  Px = np.dot( Rcom, X[0] )
-  Py = np.dot( Rcom, X[1] )
-  Pz = np.dot( Rcom, X[2] )
+    # Obtain vectors for the projection of ...
+    Px = np.dot(Rcom, X[0])
+    Py = np.dot(Rcom, X[1])
+    Pz = np.dot(Rcom, X[2])
 
-  RotExt = np.zeros((3*Natoms,3),dtype=float)
-  for i in range(3*Natoms):
-    for j in range(3):
-      RotExt[i][j] = ( Py[i]*Xp[j][2] - Pz[i]*Xp[j][1] ) / np.sqrt(masses[i])
-      RotExt[i][j] = ( Pz[i]*Xp[j][0] - Px[i]*Xp[j][2] ) / np.sqrt(masses[i])
-      RotExt[i][j] = ( Px[i]*Xp[j][1] - Py[i]*Xp[j][0] ) / np.sqrt(masses[i])
+    RotExt = np.zeros((3*Natoms,3),dtype=float)
+    for i in range(3*Natoms):
+        for j in range(3):
+            RotExt[i][j] = (Py[i]*Xp[j][2] - Pz[i]*Xp[j][1]) / np.sqrt(masses[i])
+            RotExt[i][j] = (Pz[i]*Xp[j][0] - Px[i]*Xp[j][2]) / np.sqrt(masses[i])
+            RotExt[i][j] = (Px[i]*Xp[j][1] - Py[i]*Xp[j][0]) / np.sqrt(masses[i])
 
-  return RotExt
+    return RotExt
 
 
 def build_hind_rotor_vectors(xyz):
-  ''' Builds the vectors that describe the rotaiton of a hindered rotor. 
-  '''    
+    """ Builds the vectors that describe the rotaiton of a hindered rotor. 
+    """    
 
-  # Initialize list to hold all of the Rotor vectors
-  Nrotors = 0
-  Rotors = []
+    # Initialize list to hold all of the Rotor vectors
+    Nrotors = 0
+    Rotors = []
 
-  for i in range(Nrotors):
+    for i in range(Nrotors):
 
-    # Numbers of the pivot atoms
-    pivotA = 
-    pivotB =   
+        # Numbers of the pivot atoms
+        pivotA = 
+        pivotB =   
   
     # Numbers denoting atoms attached to pivot atoms A and B 
     attA = []
@@ -117,8 +119,8 @@ def build_hind_rotor_vectors(xyz):
 
 
 def ortho_D_matrix():
-  ''' Orthogonalize the matrix describing the projected motions. 
-  '''
+  """ Orthogonalize the matrix describing the projected motions. 
+  """
   return None
 
 
--- lib/rate_constant.py
+++ lib/rate_constant.py
@@ -1,7 +1,7 @@
-''' 
+""" 
 Calculates the rate constant
   SUPPORTED: Canoncial Transition State Theory, Microcanonical RRKM Theory
-'''
+"""
 
 import numpy as np
 
@@ -9,17 +9,20 @@ from constants import kB, h
 
 
 def calc_canon_tst_rate_constant(T, Q_ts, Q_reac, barrier, kappa_T):
-  ''' Calculates the Rate Constant using Canonical Transition State Theory. '''
+    """ Calculates the Rate Constant using 
+        Canonical Transition State Theory. 
+    """
 
-  k_T =  kappa_T * ((kB*T)/h) * (Q_ts/Q_reac) * np.exp(-barrier/ (kB*T) 
+    k_T =  kappa_T * ((kB*T)/h) * (Q_ts/Q_reac) * np.exp(-barrier/ (kB*T) 
 
-  return k_T
+    return k_T
 
 
 def calc_rrkm_ke_rate_constant(N_E, p_E):
-  ''' Calculates the Rate Constant using Microcanonical Transition State Theory. '''
+    """ Calculates the Rate Constant 
+        using Microcanonical Transition State Theory. 
+    """
 
-  k_E =  N_E / (h*p_E)   
+    k_E =  N_E / (h*p_E)   
 
-  return k_E
-
+    return k_E
